921471d0dd89b55b43a5c26c0c5bd5ceb4b8831d
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-03 23:46:56 -0300

    melhora do init.sql

--- rinha-backend/src/main/resources/init.sql
+++ rinha-backend/src/main/resources/init.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS public.pessoa;
 
-create extension pgcrypto;
-
-CREATE TABLE public.pessoa (
+CREATE TABLE IF NOT EXISTS public.pessoa (
 	id uuid PRIMARY KEY  DEFAULT gen_random_uuid(),
 	nome VARCHAR(100) NOT NULL,
 	apelido VARCHAR(32) NOT NULL,
@@ -12,6 +10,6 @@ CREATE TABLE public.pessoa (
 	    lower(nome || ' ' || apelido || ' ' || COALESCE(stack,''))
 	) stored
 );
-CREATE EXTENSION PG_TRGM;
+CREATE EXTENSION IF NOT EXISTS PG_TRGM;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS pessoas_busca_completa_idx ON pessoa USING GIST (busca_completa GIST_TRGM_OPS(SIGLEN=128));
-CREATE UNIQUE INDEX pessoa_apelido_idx ON pessoa(apelido);
+CREATE UNIQUE INDEX IF NOT EXISTS pessoa_apelido_idx ON pessoa(apelido);
